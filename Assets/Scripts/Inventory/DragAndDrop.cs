a0e1e8f6f7301a931737e48db5a11a7169aaf465
PrimeLockerbee <bradleyvanewijk@gmail.com>
2022-03-21 18:53:51 +0100

    Started working on the drag and drop and the inventory

---
+++ Assets/Scripts/Inventory/DragAndDrop.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    [SerializeField] private Canvas c_Canvas;
+
+    private RectTransform rt_rectTransform;
+
+    private void Awake()
+    {
+        rt_rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rt_rectTransform.anchoredPosition += eventData.delta/ c_Canvas.scaleFactor;
+    }
+}
