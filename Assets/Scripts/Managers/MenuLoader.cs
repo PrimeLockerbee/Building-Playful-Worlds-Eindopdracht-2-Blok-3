1005a92ec8cfe58e18d88ada25bd355200c2d4bc
PrimeLockerbee <bradleyvanewijk@gmail.com>
2022-03-24 22:08:06 +0100

    Added a settings menu, next up I have to finish it and make it into a prefab. And then put it in the main scene.

--- Assets/Scripts/Managers/MenuLoader.cs
+++ Assets/Scripts/Managers/MenuLoader.cs
@@ -4,14 +4,71 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MenuLoader : MonoBehaviour
 {
     public static MenuLoader Instance;
 
+    [SerializeField] private AudioMixer am_AudioMixer;
+
+    [SerializeField] private TMP_Dropdown dd_DropDown;
+
+    Resolution[] r_Resolutions;
+
+    private int _currentResolutionIndex = 0;
+
     private void Awake()
     {
         Instance = this;
+    }
+
+    private void Start()
+    {
+        r_Resolutions = Screen.resolutions;
+
+        dd_DropDown.ClearOptions();
+
+        List<string> _options = new List<string>();
+
+
+        for (int i = 0; i< r_Resolutions.Length; i++)
+        {
+            string _option = r_Resolutions[i].width + " x " + r_Resolutions[i].height;
+            _options.Add(_option);
+
+            if(r_Resolutions[i].width == Screen.currentResolution.width && r_Resolutions[i].height == Screen.currentResolution.height)
+            {
+                _currentResolutionIndex = i;
+            }
+        }
+
+        dd_DropDown.AddOptions(_options);
+        dd_DropDown.value = _currentResolutionIndex;
+        dd_DropDown.RefreshShownValue();
+
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = r_Resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetVolume(float volume)
+    {
+        am_AudioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
     }
 
     //Loads a scene according to name
