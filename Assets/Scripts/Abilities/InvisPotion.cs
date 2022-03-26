93d4d0276e4330f1f1973edb30ebb77dc31e760b
PrimeLockerbee <bradleyvanewijk@gmail.com>
2022-03-26 10:22:41 +0100

    Invis ability works! Next up is saving and loading.

--- Assets/Scripts/Abilities/InvisPotion.cs
+++ Assets/Scripts/Abilities/InvisPotion.cs
@@ -1,20 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InvisPotion : MonoBehaviour
 {
-    [SerializeField] SpriteRenderer sr_PlayerRenderer;
+    SpriteRenderer sr_PlayerRenderer;
+    Button b_InvisButton;
+    bool b_isInvis;
 
     private void Start()
     {
-        sr_PlayerRenderer = GameObject.Find("Player").GetComponent<SpriteRenderer>();
+        sr_PlayerRenderer = GameObject.Find("Player(Clone)").GetComponent<SpriteRenderer>();
+        b_InvisButton = GetComponent<Button>();
+        b_isInvis = false;
     }
 
     public void InvisActivate()
     {
+        if(b_isInvis == false)
+        {
+            Color c_temp = sr_PlayerRenderer.color;
+            c_temp.a = .5f;
+            sr_PlayerRenderer.color = c_temp;
+
+            StartCoroutine(LockInvis());
+            StartCoroutine(LockButton());
+        }
+    }
+
+    IEnumerator LockInvis()
+    {
+        b_isInvis = true;
+
+
+        yield return new WaitForSeconds(5);
+
         Color c_temp = sr_PlayerRenderer.color;
-        c_temp.a = 0f;
+        c_temp.a = 1f;
         sr_PlayerRenderer.color = c_temp;
+
+
+
+        b_isInvis = false;
+    }
+
+    IEnumerator LockButton()
+    {
+        b_InvisButton.interactable = false;
+
+        yield return new WaitForSeconds(15);
+
+        b_InvisButton.interactable = true;
     }
 }
